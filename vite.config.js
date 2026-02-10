aa3a147302b3ff88985356f5afc33215d441e4c2
Harsha Yenugutala <harshayenuguthala904@gmail.com>
2026-02-10 13:51:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sem-2-project-4/"
+  base: "/project-6/"
 })
